e03219c1c84f6f24c64115a705b824695022e029
Leonan Marques <leonan.marques886@gmail.com>
2023-12-17 12:50:12 -0300

    the dot class have been implemented

---
+++ implementation/src/structural/composite/Dot.java
@@ -0,0 +1,22 @@
+package structural.composite;
+
+public class Dot implements Graphic {
+    private int x;
+    private int y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing dot at (" + x + ", " + y + ")");
+    }
+}
